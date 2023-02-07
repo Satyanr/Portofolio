01d3229b722dfcb71811f0cc05447595976d61c8
Satyanr <90590677+Satyanr@users.noreply.github.com>
2023-02-07 19:53:44 +0700

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+AOS.init();
+(function() {
+  "use strict";
+
+    var typed = new Typed(".typed",{
+    strings: [,  "Designer", "Developer", "Freelancer", "Photographer"] ,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+
+    })
+
+
+})()
